888af2c0b94932b1ba2ab31c7c3dc3480833a4fb
MRomu <romuald.millard@gmail.com>
2018-02-05 11:10:03 -0500

    Team project

---
+++ Bates Reservation/Projet_0/Fo_0.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Projet_0
+{
+    partial class Fo_0
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fo_0));
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1215, 576);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 72);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 210);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1197, 590);
+            this.panel1.TabIndex = 13;
+            // 
+            // button11
+            // 
+            this.button11.Image = global::Projet_0.Properties.Resources.bt_fw;
+            this.button11.Location = new System.Drawing.Point(460, 806);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 63);
+            this.button11.TabIndex = 15;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Projet_0.Properties.Resources.file_new;
+            this.button1.Location = new System.Drawing.Point(418, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ajouter";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Image = global::Projet_0.Properties.Resources.bt_skip_fw;
+            this.button12.Location = new System.Drawing.Point(379, 807);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 63);
+            this.button12.TabIndex = 16;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Projet_0.Properties.Resources.Logo;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 192);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button13
+            // 
+            this.button13.Image = global::Projet_0.Properties.Resources.bt_skip_rw;
+            this.button13.Location = new System.Drawing.Point(298, 806);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 63);
+            this.button13.TabIndex = 17;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Image = global::Projet_0.Properties.Resources.bt_rw;
+            this.button10.Location = new System.Drawing.Point(217, 806);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 63);
+            this.button10.TabIndex = 14;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::Projet_0.Properties.Resources.file_explore;
+            this.button3.Location = new System.Drawing.Point(622, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 66);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Consulter";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::Projet_0.Properties.Resources.file_del;
+            this.button5.Location = new System.Drawing.Point(554, 138);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 66);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Supprimer";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::Projet_0.Properties.Resources.file_edit;
+            this.button4.Location = new System.Drawing.Point(486, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(62, 66);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Modifier";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Image = global::Projet_0.Properties.Resources.bt_remove;
+            this.button9.Location = new System.Drawing.Point(1215, 728);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 72);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Image = global::Projet_0.Properties.Resources.save_as;
+            this.button7.Location = new System.Drawing.Point(1215, 408);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 64);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::Projet_0.Properties.Resources.save;
+            this.button6.Location = new System.Drawing.Point(1215, 210);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 67);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(1090, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // Fo_0
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1301, 882);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Fo_0";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Bates Reservation/Projet_0/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Projet_0
+{
+    partial class Fo_0
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1_G = new System.Windows.Forms.Panel();
+            this.panel2_G = new System.Windows.Forms.Panel();
+            this.dateTimePicker_G = new System.Windows.Forms.DateTimePicker();
+            this.label_version_G = new System.Windows.Forms.Label();
+            this.label_numversion_G = new System.Windows.Forms.Label();
+            this.button_undo_G = new System.Windows.Forms.Button();
+            this.button_save_G = new System.Windows.Forms.Button();
+            this.button_list_G = new System.Windows.Forms.Button();
+            this.button_gotoend_G = new System.Windows.Forms.Button();
+            this.button_add_G = new System.Windows.Forms.Button();
+            this.button_next_G = new System.Windows.Forms.Button();
+            this.button_previous_G = new System.Windows.Forms.Button();
+            this.button_gotofirst_G = new System.Windows.Forms.Button();
+            this.button_consult_G = new System.Windows.Forms.Button();
+            this.button_del_G = new System.Windows.Forms.Button();
+            this.button_mod_G = new System.Windows.Forms.Button();
+            this.button_exit_G = new System.Windows.Forms.Button();
+            this.pictureBox_G = new System.Windows.Forms.PictureBox();
+            this.panel1_G.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_G)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1_G
+            // 
+            this.panel1_G.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1_G.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1_G.Controls.Add(this.panel2_G);
+            this.panel1_G.Controls.Add(this.label_numversion_G);
+            this.panel1_G.Controls.Add(this.label_version_G);
+            this.panel1_G.Location = new System.Drawing.Point(12, 143);
+            this.panel1_G.Name = "panel1_G";
+            this.panel1_G.Size = new System.Drawing.Size(1540, 591);
+            this.panel1_G.TabIndex = 13;
+            // 
+            // panel2_G
+            // 
+            this.panel2_G.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2_G.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2_G.Location = new System.Drawing.Point(14, 13);
+            this.panel2_G.Name = "panel2_G";
+            this.panel2_G.Size = new System.Drawing.Size(1513, 566);
+            this.panel2_G.TabIndex = 0;
+            // 
+            // dateTimePicker_G
+            // 
+            this.dateTimePicker_G.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker_G.CustomFormat = "yyyy/mm/dd";
+            this.dateTimePicker_G.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_G.Location = new System.Drawing.Point(1409, 12);
+            this.dateTimePicker_G.Name = "dateTimePicker_G";
+            this.dateTimePicker_G.Size = new System.Drawing.Size(143, 20);
+            this.dateTimePicker_G.TabIndex = 20;
+            // 
+            // label_version_G
+            // 
+            this.label_version_G.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label_version_G.AutoSize = true;
+            this.label_version_G.ForeColor = System.Drawing.SystemColors.Window;
+            this.label_version_G.Location = new System.Drawing.Point(1411, 0);
+            this.label_version_G.Name = "label_version_G";
+            this.label_version_G.Size = new System.Drawing.Size(42, 13);
+            this.label_version_G.TabIndex = 21;
+            this.label_version_G.Text = "Version";
+            // 
+            // label_numversion_G
+            // 
+            this.label_numversion_G.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label_numversion_G.AutoSize = true;
+            this.label_numversion_G.Location = new System.Drawing.Point(1489, 0);
+            this.label_numversion_G.Name = "label_numversion_G";
+            this.label_numversion_G.Size = new System.Drawing.Size(35, 13);
+            this.label_numversion_G.TabIndex = 22;
+            this.label_numversion_G.Text = "label2";
+            // 
+            // button_undo_G
+            // 
+            this.button_undo_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_undo_G.Image = global::Projet_0.Properties.Resources.document_revert;
+            this.button_undo_G.Location = new System.Drawing.Point(691, 44);
+            this.button_undo_G.Name = "button_undo_G";
+            this.button_undo_G.Size = new System.Drawing.Size(85, 93);
+            this.button_undo_G.TabIndex = 24;
+            this.button_undo_G.Text = "Annuler";
+            this.button_undo_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_undo_G.UseVisualStyleBackColor = true;
+            // 
+            // button_save_G
+            // 
+            this.button_save_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_save_G.Image = global::Projet_0.Properties.Resources.document_save1;
+            this.button_save_G.Location = new System.Drawing.Point(509, 44);
+            this.button_save_G.Name = "button_save_G";
+            this.button_save_G.Size = new System.Drawing.Size(85, 93);
+            this.button_save_G.TabIndex = 23;
+            this.button_save_G.Text = "Sauvegarder";
+            this.button_save_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_save_G.UseVisualStyleBackColor = true;
+            // 
+            // button_list_G
+            // 
+            this.button_list_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_list_G.Image = global::Projet_0.Properties.Resources.system_search1;
+            this.button_list_G.Location = new System.Drawing.Point(964, 44);
+            this.button_list_G.Name = "button_list_G";
+            this.button_list_G.Size = new System.Drawing.Size(85, 93);
+            this.button_list_G.TabIndex = 19;
+            this.button_list_G.Text = "LIster";
+            this.button_list_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_list_G.UseVisualStyleBackColor = true;
+            // 
+            // button_gotoend_G
+            // 
+            this.button_gotoend_G.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button_gotoend_G.Image = global::Projet_0.Properties.Resources.go_last;
+            this.button_gotoend_G.Location = new System.Drawing.Point(830, 740);
+            this.button_gotoend_G.Name = "button_gotoend_G";
+            this.button_gotoend_G.Size = new System.Drawing.Size(75, 63);
+            this.button_gotoend_G.TabIndex = 15;
+            this.button_gotoend_G.UseVisualStyleBackColor = true;
+            // 
+            // button_add_G
+            // 
+            this.button_add_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_add_G.Image = global::Projet_0.Properties.Resources.address_book_new;
+            this.button_add_G.Location = new System.Drawing.Point(418, 44);
+            this.button_add_G.Name = "button_add_G";
+            this.button_add_G.Size = new System.Drawing.Size(85, 93);
+            this.button_add_G.TabIndex = 0;
+            this.button_add_G.Text = "Ajouter";
+            this.button_add_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_add_G.UseVisualStyleBackColor = true;
+            // 
+            // button_next_G
+            // 
+            this.button_next_G.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button_next_G.Image = global::Projet_0.Properties.Resources.go_next;
+            this.button_next_G.Location = new System.Drawing.Point(749, 740);
+            this.button_next_G.Name = "button_next_G";
+            this.button_next_G.Size = new System.Drawing.Size(75, 63);
+            this.button_next_G.TabIndex = 16;
+            this.button_next_G.UseVisualStyleBackColor = true;
+            // 
+            // button_previous_G
+            // 
+            this.button_previous_G.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button_previous_G.Image = global::Projet_0.Properties.Resources.go_previous;
+            this.button_previous_G.Location = new System.Drawing.Point(657, 740);
+            this.button_previous_G.Name = "button_previous_G";
+            this.button_previous_G.Size = new System.Drawing.Size(75, 63);
+            this.button_previous_G.TabIndex = 17;
+            this.button_previous_G.UseVisualStyleBackColor = true;
+            // 
+            // button_gotofirst_G
+            // 
+            this.button_gotofirst_G.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button_gotofirst_G.Image = global::Projet_0.Properties.Resources.go_first;
+            this.button_gotofirst_G.Location = new System.Drawing.Point(576, 740);
+            this.button_gotofirst_G.Name = "button_gotofirst_G";
+            this.button_gotofirst_G.Size = new System.Drawing.Size(75, 63);
+            this.button_gotofirst_G.TabIndex = 14;
+            this.button_gotofirst_G.UseVisualStyleBackColor = true;
+            // 
+            // button_consult_G
+            // 
+            this.button_consult_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_consult_G.Image = global::Projet_0.Properties.Resources.edit_find;
+            this.button_consult_G.Location = new System.Drawing.Point(873, 44);
+            this.button_consult_G.Name = "button_consult_G";
+            this.button_consult_G.Size = new System.Drawing.Size(85, 93);
+            this.button_consult_G.TabIndex = 10;
+            this.button_consult_G.Text = "Consulter";
+            this.button_consult_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_consult_G.UseVisualStyleBackColor = true;
+            // 
+            // button_del_G
+            // 
+            this.button_del_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_del_G.Image = global::Projet_0.Properties.Resources.application_exit;
+            this.button_del_G.Location = new System.Drawing.Point(782, 44);
+            this.button_del_G.Name = "button_del_G";
+            this.button_del_G.Size = new System.Drawing.Size(85, 93);
+            this.button_del_G.TabIndex = 12;
+            this.button_del_G.Text = "Suprimer";
+            this.button_del_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_del_G.UseVisualStyleBackColor = true;
+            // 
+            // button_mod_G
+            // 
+            this.button_mod_G.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button_mod_G.Image = global::Projet_0.Properties.Resources.document_edit;
+            this.button_mod_G.Location = new System.Drawing.Point(600, 44);
+            this.button_mod_G.Name = "button_mod_G";
+            this.button_mod_G.Size = new System.Drawing.Size(85, 93);
+            this.button_mod_G.TabIndex = 11;
+            this.button_mod_G.Text = "Modifier";
+            this.button_mod_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_mod_G.UseVisualStyleBackColor = true;
+            // 
+            // button_exit_G
+            // 
+            this.button_exit_G.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button_exit_G.Image = global::Projet_0.Properties.Resources.process_stop;
+            this.button_exit_G.Location = new System.Drawing.Point(1477, 740);
+            this.button_exit_G.Name = "button_exit_G";
+            this.button_exit_G.Size = new System.Drawing.Size(75, 72);
+            this.button_exit_G.TabIndex = 8;
+            this.button_exit_G.Text = "Exit";
+            this.button_exit_G.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_exit_G.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox_G
+            // 
+            this.pictureBox_G.Image = global::Projet_0.Properties.Resources.Logo;
+            this.pictureBox_G.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_G.Name = "pictureBox_G";
+            this.pictureBox_G.Size = new System.Drawing.Size(400, 192);
+            this.pictureBox_G.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_G.TabIndex = 18;
+            this.pictureBox_G.TabStop = false;
+            // 
+            // Fo_0
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1564, 819);
+            this.Controls.Add(this.button_undo_G);
+            this.Controls.Add(this.button_save_G);
+            this.Controls.Add(this.dateTimePicker_G);
+            this.Controls.Add(this.button_list_G);
+            this.Controls.Add(this.button_gotoend_G);
+            this.Controls.Add(this.button_add_G);
+            this.Controls.Add(this.button_next_G);
+            this.Controls.Add(this.button_previous_G);
+            this.Controls.Add(this.button_gotofirst_G);
+            this.Controls.Add(this.button_consult_G);
+            this.Controls.Add(this.button_del_G);
+            this.Controls.Add(this.panel1_G);
+            this.Controls.Add(this.button_mod_G);
+            this.Controls.Add(this.button_exit_G);
+            this.Controls.Add(this.pictureBox_G);
+            this.Name = "Fo_0";
+            this.Text = "Form1";
+            this.panel1_G.ResumeLayout(false);
+            this.panel1_G.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_G)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_add_G;
+        private System.Windows.Forms.Button button_exit_G;
+        private System.Windows.Forms.Button button_mod_G;
+        private System.Windows.Forms.Button button_del_G;
+        private System.Windows.Forms.Panel panel1_G;
+        private System.Windows.Forms.Button button_gotofirst_G;
+        private System.Windows.Forms.Button button_gotoend_G;
+        private System.Windows.Forms.Button button_next_G;
+        private System.Windows.Forms.Button button_previous_G;
+        private System.Windows.Forms.PictureBox pictureBox_G;
+        private System.Windows.Forms.Button button_consult_G;
+        private System.Windows.Forms.Button button_list_G;
+        private System.Windows.Forms.Panel panel2_G;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_G;
+        private System.Windows.Forms.Label label_version_G;
+        private System.Windows.Forms.Label label_numversion_G;
+        private System.Windows.Forms.Button button_save_G;
+        private System.Windows.Forms.Button button_undo_G;
+    }
+}
+
